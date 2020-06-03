cfed0b4ee761332b88dce409b2e8295ced23818e
Xuejie Xiao <xxuejie@gmail.com>
2020-06-03 05:43:38 +0000

    fix: Deploy script

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,15 +15,15 @@ const siteConfig = {
 
   title: 'Nervos CKB Docs', // Title for your website.
   tagline: 'This is the documentation site for Nervos CKB',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://nervosnetwork.github.io', // Your website URL
+  baseUrl: '/docs-new/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
-  organizationName: 'Nervos',
+  projectName: 'docs-new',
+  organizationName: 'nervosnetwork',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
