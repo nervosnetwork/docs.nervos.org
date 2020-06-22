2fb0677ec6e971fc1eb2cf77d486841be777ce1c
James Chen <james@ashchan.com>
2020-06-22 14:36:08 +0900

    Change base url from docs-new/ to /

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,7 +16,7 @@ const siteConfig = {
   title: 'Nervos CKB Docs', // Title for your website.
   tagline: 'This is the documentation site for Nervos CKB',
   url: 'https://nervosnetwork.github.io', // Your website URL
-  baseUrl: '/docs-new/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
