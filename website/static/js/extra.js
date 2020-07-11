2b72b7281b577e0014aab355b53ba16af19065e9
Jordan Mack <jordan.mack@brilliantnotion.com>
2020-07-10 23:26:33 -0700

    Reattach old site banner as soon as DOM is loaded.

--- website/static/js/extra.js
+++ website/static/js/extra.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', function() {
   var banner = document.querySelector('#oldSiteLink');
   document.querySelector('body').prepend(banner);
 });
