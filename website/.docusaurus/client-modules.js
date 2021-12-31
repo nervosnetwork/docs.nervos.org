a2528ce4a7c7f27f250087aad7b2b4736efcb44f
homura <homura.dev@gmail.com>
2021-12-31 20:55:29 +0800

    chore: remove unnecessary files

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/cary/20211215/docs/v2-website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/cary/20211215/docs/v2-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/cary/20211215/docs/v2-website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("../src/css/customTheme.css"),
-];
