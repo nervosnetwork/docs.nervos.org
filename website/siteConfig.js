0e68a3a9f7c48a2f82b93300ba425163f76b331f
caoliang <caoliang@yodo1.com>
2022-01-05 21:42:22 +0800

    add lock file

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2017-present, Facebook, Inc.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE file in the root directory of this source tree..
  */
 
 // See https://docusaurus.io/docs/site-config for all the possible
