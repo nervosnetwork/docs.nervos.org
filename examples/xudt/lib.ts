2e1703213419085142938cc7913ef06217235736
RetricSu <inneverland2013@gmail.com>
2024-03-20 13:11:52 +0800

    chore: rm console.log

--- examples/xudt/lib.ts
+++ examples/xudt/lib.ts
@@ -14,7 +14,6 @@ export async function issueToken(privKey: string, amount: string) {
   const { lockScript } = generateAccountFromPrivateKey(privKey);
   const xudtArgs = utils.computeScriptHash(lockScript) + '00000000';
 
-  console.log(lumosConfig, xudtDeps);
   const typeScript = {
     codeHash: xudtDeps.CODE_HASH,
     hashType: xudtDeps.HASH_TYPE,
