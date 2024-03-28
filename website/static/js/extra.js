808e089cf597bbf6cd48f9081b6f282a5817b0f9
RetricSu <inneverland2013@gmail.com>
2024-03-28 10:46:03 +0800

    chore: redirect old-doc to v1 vercel

--- website/static/js/extra.js
+++ website/static/js/extra.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const oldSiteLink = document.createElement("div");
   oldSiteLink.innerHTML = `<div id="oldSiteLink">
                     <div>
-                        <span>Note we've completely rebuilt Nervos Doc site! </span><span>For the old doc site, please see <a href="https://docs-old.nervos.org">docs-old</a>.</span>
+                        <span>Note we've completely rebuilt Nervos Doc site! </span><span>For the old doc site, please see <a href="https://nervos-ckb-docs-git-v1-cryptape.vercel.app/">docs-old</a>.</span>
                     </div>
                 </div>`;
   document.querySelector("body").prepend(oldSiteLink);
