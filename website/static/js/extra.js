82324aeca22e6812979e11df26012217501bbeec
RetricSu <inneverland2013@gmail.com>
2024-03-28 10:40:18 +0800

    chore: add new doc link

--- website/static/js/extra.js
+++ website/static/js/extra.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const oldSiteLink = document.createElement("div");
   oldSiteLink.innerHTML = `<div id="oldSiteLink">
                     <div>
-                        <span>Note we've completely rebuilt Nervos Doc site! </span><span>For the old doc site, please see <a href="https://docs-old.nervos.org">docs-old</a>.</span>
+                        <span>Note we've completely rebuilt Nervos Doc site! </span><span>For the new doc site, please see <a href="https://nervos-ckb-docs-git-develop-v2-cryptape.vercel.app/">docs-new</a>.</span>
                     </div>
                 </div>`;
   document.querySelector("body").prepend(oldSiteLink);
