904163bc4ebfd549aefa20b3195fb736cae17891
Yuqi Feng <yfeng@cryptape.com>
2024-04-15 17:48:55 +0800

    add links

--- website/src/pages/homeContents.tsx
+++ website/src/pages/homeContents.tsx
@@ -19,7 +19,7 @@ const homeCardContents: HomeCardProps[] = [
         links: [
             { label: "Dev Environment", link: "/docs/getting-started/dev-environment"},
             { label: "DApp Tutorials", link: "/docs/getting-started/transfer-ckb"},
-            { label: "Scripts Tutorials", link: "/docs/Script/basic-script"},
+            { label: "Scripts Tutorials", link: "/docs/Script/write-script"},
         ],
         icon: 'rocket',
     },
