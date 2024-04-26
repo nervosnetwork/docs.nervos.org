38ce994ae7c3dd8d39ea4456d8fc238fc4482541
RetricSu <inneverland2013@gmail.com>
2024-04-26 15:28:18 +0800

    fix: auto redirect old docs link (#310)

--- website/src/theme/NotFound/Content/index.js
+++ website/src/theme/NotFound/Content/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "@docusaurus/router";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./styles.module.css";
+import redirects from "./redirect.json";
 
 // according to docusaurus, unwrap NotFoundContent is danger
 // we can undo this after there is no broken link from v1
@@ -9,6 +10,29 @@ export default function ContentWrapper(props) {
   const location = useLocation();
   const { siteConfig } = useDocusaurusContext();
   const oldDocUrl = siteConfig.customFields.oldDocSiteUrl;
+
+  const handleRedirect = (pathname) => {
+    const shouldRedirect = redirects.some((pattern) =>
+      pathname.includes(pattern)
+    );
+
+    if (shouldRedirect) {
+      // send to analytics
+      window.gtag &&
+        window.gtag("event", "auto-redirect", {
+          event_category: "Site Auto Redirects",
+          event_label: pathname,
+        });
+
+      // redirect to old docs
+      window.location.href = `${oldDocUrl}${pathname}`;
+    }
+  };
+
+  useEffect(() => {
+    handleRedirect(location.pathname);
+  }, [location.pathname, oldDocUrl]);
+
   return (
     <div className={styles.notFoundContainer}>
       <h1>Page Not Found</h1>
