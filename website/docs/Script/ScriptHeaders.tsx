b05e0790413058b8d31290516412dbcdd509a4d7
RetricSu <inneverland2013@gmail.com>
2024-04-26 15:28:44 +0800

    update clang 16+ install in overview (#312)

--- website/docs/Script/ScriptHeaders.tsx
+++ website/docs/Script/ScriptHeaders.tsx
@@ -18,20 +18,80 @@ export const ScriptHeaders: ScriptHeadersType = {
     ],
     tools: [
       <div>
-        <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">git</a>,
-        <a href="https://www.tutorialspoint.com/unix_commands/make.htm" target="_blank" rel="noopener noreferrer">make</a>,
-        <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noopener noreferrer">bash</a>,
-        <a href="https://linux.die.net/man/1/sha256sum" target="_blank" rel="noopener noreferrer">sha256sum</a> 
-        {" and others Unix utilities."}</div>,
+        <a
+          href="https://git-scm.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          git
+        </a>
+        ,
+        <a
+          href="https://www.tutorialspoint.com/unix_commands/make.htm"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          make
+        </a>
+        ,
+        <a
+          href="https://www.gnu.org/software/bash/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          bash
+        </a>
+        ,
+        <a
+          href="https://linux.die.net/man/1/sha256sum"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          sha256sum
+        </a>
+        {" and others Unix utilities."}
+      </div>,
       <div>
         <a href={"https://www.rust-lang.org/"}>Rust</a>
-        {" and riscv64 target: "} <code>rustup target add riscv64imac-unknown-none-elf</code>
+        {" and riscv64 target: "}{" "}
+        <code>rustup target add riscv64imac-unknown-none-elf</code>
       </div>,
       <div>
-        <a href={"https://releases.llvm.org/16.0.0/tools/clang/docs/ReleaseNotes.html"}>Clang 16+</a>
+        <a
+          href={
+            "https://releases.llvm.org/16.0.0/tools/clang/docs/ReleaseNotes.html"
+          }
+        >
+          Clang 16+
+        </a>
+        {" sample installtion steps for selected platforms are:"}
+        <ul>
+        <li>
+          Debian / Ubuntu:{" "}
+          <code>
+            wget https://apt.llvm.org/llvm.sh && chmod +x llvm.sh && sudo
+            ./llvm.sh 16 && rm llvm.sh
+          </code>
+        </li>
+        <li>
+          Fedora 39+: <code>sudo dnf -y install clang</code>
+        </li>
+        <li>
+          Archlinux: <code>sudo pacman --noconfirm -Syu clang</code>
+        </li>
+        <li>
+          macOS: <code>brew install llvm@16</code>
+        </li>
+        <li>
+          Windows(with [Scoop](scoop install llvm yasm)):{" "}
+          <code>scoop install llvm yasm</code>
+        </li>
+        </ul>
       </div>,
       <div>
-        <a href="https://github.com/cargo-generate/cargo-generate">cargo-generate</a>
+        <a href="https://github.com/cargo-generate/cargo-generate">
+          cargo-generate
+        </a>
       </div>,
     ],
   },
