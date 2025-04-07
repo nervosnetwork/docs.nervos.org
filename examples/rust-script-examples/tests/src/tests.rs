527e24a62c75695a28339df9ce0fc4958072beb2
joii2020 <joii@cryptape.com>
2025-04-07 10:59:05 +0800

    Add testcase: rust examples empty

--- examples/rust-script-examples/tests/src/tests.rs
+++ examples/rust-script-examples/tests/src/tests.rs
@@ -57,6 +57,57 @@ fn test_hello_world() {
     println!("consume cycles: {}", cycles);
 }
 
+#[test]
+fn test_empty() {
+    // deploy contract
+    let mut context = Context::default();
+    let contract_bin: Bytes = Loader::default().load_binary("empty");
+    let out_point = context.deploy_cell(contract_bin);
+
+    // prepare scripts
+    let lock_script = context
+        .build_script(&out_point, Bytes::from(vec![42]))
+        .expect("script");
+
+    // prepare cells
+    let input_out_point = context.create_cell(
+        CellOutput::new_builder()
+            .capacity(1000u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        Bytes::new(),
+    );
+    let input = CellInput::new_builder()
+        .previous_output(input_out_point)
+        .build();
+    let outputs = vec![
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script)
+            .build(),
+    ];
+
+    let outputs_data = vec![Bytes::new(); 2];
+
+    // build transaction
+    let tx = TransactionBuilder::default()
+        .input(input)
+        .outputs(outputs)
+        .outputs_data(outputs_data.pack())
+        .build();
+    let tx = context.complete_tx(tx);
+
+    // run
+    let cycles = context
+        .verify_tx(&tx, 10_000_000)
+        .expect("pass verification");
+    println!("consume cycles: {}", cycles);
+}
+
 // generated unit test for contract simple-print-args
 #[test]
 fn test_simple_print_args() {
@@ -107,9 +158,9 @@ fn test_simple_print_args() {
             .expect("tx format json")
     );
 
-// run
-let cycles = context
-    .verify_tx(&tx, 10_000_000)
-    .expect("pass verification");
-println!("consume cycles: {}", cycles);
+    // run
+    let cycles = context
+        .verify_tx(&tx, 10_000_000)
+        .expect("pass verification");
+    println!("consume cycles: {}", cycles);
 }
