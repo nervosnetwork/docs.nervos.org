648e6646b888e1d424c0b73771bccc2e080982e3
joii2020 <joii@cryptape.com>
2025-04-17 17:30:22 +0800

    Rust test: add Deterministic tx

--- examples/rust-script-examples/tests/src/test_syscalls.rs
+++ examples/rust-script-examples/tests/src/test_syscalls.rs
@@ -115,3 +115,114 @@ fn test_script_hash_in_type() {
         .expect("pass verification");
     println!("consume cycles: {}", cycles);
 }
+
+#[test]
+fn test_tx_hash() {
+    // deploy contract
+    let mut context = Context::default();
+    let contract_bin: Bytes = Loader::default().load_binary("tx-hash");
+    let out_point = context.deploy_cell(contract_bin);
+
+    // prepare scripts
+    let lock_script = context
+        .build_script(&out_point, Bytes::from(vec![42]))
+        .expect("script");
+
+    // prepare cells
+    let input_out_point = context.create_cell(
+        CellOutput::new_builder()
+            .capacity(1000u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        Bytes::new(),
+    );
+    let input = CellInput::new_builder()
+        .previous_output(input_out_point)
+        .build();
+    let outputs = vec![
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script)
+            .build(),
+    ];
+
+    let outputs_data = vec![Bytes::new(); 2];
+
+    // build transaction
+    let tx = TransactionBuilder::default()
+        .input(input)
+        .outputs(outputs)
+        .outputs_data(outputs_data.pack())
+        .build();
+    let tx = context.complete_tx(tx);
+
+    let tx_hash = tx.hash().as_bytes();
+    let tx = tx.as_advanced_builder().witness(tx_hash.pack()).build();
+
+    // run
+    let cycles = context
+        .verify_tx(&tx, 10_000_000)
+        .expect("pass verification");
+    println!("consume cycles: {}", cycles);
+}
+
+#[test]
+fn test_fix_tx_hash() {
+    // deploy contract
+    let mut context = Context::new_with_deterministic_rng();
+    let contract_bin: Bytes = Loader::default().load_binary("tx-hash");
+    let out_point = context.deploy_cell(contract_bin);
+
+    // prepare scripts
+    let lock_script = context
+        .build_script(&out_point, Bytes::from(vec![42]))
+        .expect("script");
+
+    // prepare cells
+    let input_out_point = OutPoint::new_builder().tx_hash([11u8; 32].pack()).build();
+    context.create_cell_with_out_point(
+        input_out_point.clone(),
+        CellOutput::new_builder()
+            .capacity(1000u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        Bytes::new(),
+    );
+
+    let input = CellInput::new_builder()
+        .previous_output(input_out_point)
+        .build();
+    let outputs = vec![
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script)
+            .build(),
+    ];
+
+    let outputs_data = vec![Bytes::new(); 2];
+
+    // build transaction
+    let tx = TransactionBuilder::default()
+        .input(input)
+        .outputs(outputs)
+        .outputs_data(outputs_data.pack())
+        .build();
+    let tx = context.complete_tx(tx);
+
+    let tx_hash = tx.hash().as_bytes();
+    let tx = tx.as_advanced_builder().witness(tx_hash.pack()).build();
+
+    // run
+    let cycles = context
+        .verify_tx(&tx, 10_000_000)
+        .expect("pass verification");
+    println!("consume cycles: {}", cycles);
+}
--- examples/rust-script-examples/tests/src/tests.rs
+++ examples/rust-script-examples/tests/src/tests.rs
@@ -165,61 +165,6 @@ fn test_simple_print_args() {
     println!("consume cycles: {}", cycles);
 }
 
-// generated unit test for contract tx-hash
-#[test]
-fn test_tx_hash() {
-    // deploy contract
-    let mut context = Context::default();
-    let contract_bin: Bytes = Loader::default().load_binary("tx-hash");
-    let out_point = context.deploy_cell(contract_bin);
-
-    // prepare scripts
-    let lock_script = context
-        .build_script(&out_point, Bytes::from(vec![42]))
-        .expect("script");
-
-    // prepare cells
-    let input_out_point = context.create_cell(
-        CellOutput::new_builder()
-            .capacity(1000u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        Bytes::new(),
-    );
-    let input = CellInput::new_builder()
-        .previous_output(input_out_point)
-        .build();
-    let outputs = vec![
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script)
-            .build(),
-    ];
-
-    let outputs_data = vec![Bytes::new(); 2];
-
-    // build transaction
-    let tx = TransactionBuilder::default()
-        .input(input)
-        .outputs(outputs)
-        .outputs_data(outputs_data.pack())
-        .build();
-    let tx = context.complete_tx(tx);
-
-    let tx_hash = tx.hash().as_bytes();
-    let tx = tx.as_advanced_builder().witness(tx_hash.pack()).build();
-
-    // run
-    let cycles = context
-        .verify_tx(&tx, 10_000_000)
-        .expect("pass verification");
-    println!("consume cycles: {}", cycles);
-}
-
 // generated unit test for contract log
 #[test]
 fn test_log() {
