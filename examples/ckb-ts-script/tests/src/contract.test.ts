d95bbcf01707959f581f819f8ec0fc75e8854560
joii2020 <joii@cryptape.com>
2025-05-14 16:49:22 +0800

    Add simple print args and test

--- examples/ckb-ts-script/tests/src/contract.test.ts
+++ examples/ckb-ts-script/tests/src/contract.test.ts
@@ -1,10 +1,11 @@
-import { DEFAULT_SCRIPT_ALWAYS_SUCCESS, Resource, Verifier } from "ckb-testtool";
-import { hashCkb, hexFrom, Hex, Transaction } from "@ckb-ccc/core";
+import { Resource, Verifier } from "ckb-testtool";
+import { hashCkb, hexFrom, Hex, Transaction, WitnessArgs } from "@ckb-ccc/core";
 import { readFileSync } from "fs";
 
 
 const CKB_JS_VM_SCRIPT = readFileSync('../deps/ckb-js-vm');
 const SCRIPT_HELLO_WORLD = readFileSync('../dist/hello-world.bc')
+const SCRIPT_SIMPLE_PRINT_ARGS = readFileSync('../dist/simple-print-args.bc')
 
 function createJSScript(resource: Resource, tx: Transaction, jsCode: Hex, args: Hex) {
     const lockScript = resource.deployCell(
@@ -19,13 +20,13 @@ function createJSScript(resource: Resource, tx: Transaction, jsCode: Hex, args: 
     );
     tx.cellDeps.push(resource.createCellDep(cell, "code"));
 
-    let code_hash = hashCkb(SCRIPT_HELLO_WORLD);
+    let code_hash = hashCkb(jsCode);
     lockScript.args = hexFrom('0x0000' + code_hash.slice(2) + '04' + args.slice(2));
 
     return lockScript;
 }
 
-test('hello-world check', () => {
+test('hello-world success', () => {
     const resource = Resource.default();
     const tx = Transaction.default();
     const lockScript = createJSScript(resource, tx, hexFrom(SCRIPT_HELLO_WORLD), "0x");
@@ -39,6 +40,31 @@ test('hello-world check', () => {
     tx.outputs.push(Resource.createCellOutput(lockScript));
     // add output data to the transaction
     tx.outputsData.push(hexFrom("0x"));
+
+    // verify the transaction
+    const verifier = Verifier.from(resource, tx);
+    verifier.verifySuccess(true);
+});
+
+test('simple-print-args success', () => {
+    const resource = Resource.default();
+    const tx = Transaction.default();
+    const lockScript = createJSScript(resource, tx, hexFrom(SCRIPT_SIMPLE_PRINT_ARGS), "0x010203040506");
+
+    // mock a input cell with the created script as lock script
+    const inputCell = resource.mockCell(lockScript);
+    const witness = WitnessArgs.from({
+        lock: "0x00112233445566"
+    });
+
+    // add input cell to the transaction
+    tx.inputs.push(Resource.createCellInput(inputCell));
+    // add output cell to the transaction
+    tx.outputs.push(Resource.createCellOutput(lockScript));
+    // add output data to the transaction
+    tx.outputsData.push(hexFrom("0x"));
+    // add witnesses
+    tx.witnesses.push(hexFrom(witness.toBytes()));
 
     // verify the transaction
     const verifier = Verifier.from(resource, tx);
