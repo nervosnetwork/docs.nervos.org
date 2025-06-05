a61ab477423c0086a635adf8daf078ccc531f76e
ahonn <yuexunjiang@gmail.com>
2025-06-05 15:49:28 +1000

    fix: improve sidebar scrolling and visibility checks

--- website/static/js/scrollSidebar.js
+++ website/static/js/scrollSidebar.js
@@ -1,25 +1,61 @@
-function ensureActiveTabInView() {
-  // Parent category is considered active too
-  const activeItems = document.querySelectorAll(".menu__link--active");
-  if (!activeItems || activeItems.length < 2) return;
-  // Pick the actual active tab
-  const item = activeItems[1];
-  const bounding = item.getBoundingClientRect();
+const SIDEBAR_SCROLL_TOP_KEY = "sidebarScrollTop";
+const SCROLL_THRESHOLD = 20;
 
-  if (
-    bounding.top >= 0 &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight)
-  ) {
-    // Already visible.
+function ensureActiveTabInView() {
+  const sidebar = document.querySelector("nav[aria-label='Docs sidebar']");
+
+  if (sidebar) {
+    const lastScrollTop = sessionStorage.getItem(SIDEBAR_SCROLL_TOP_KEY);
+    console.log("Restoring sidebar scroll position:", lastScrollTop);
+    if (lastScrollTop !== null) {
+      sidebar.scrollTop = parseInt(lastScrollTop, 10);
+    }
+  }
+
+  const activeItems = document.querySelectorAll(".menu__link--active");
+
+  if (!activeItems || activeItems.length === 0) {
+    if (sidebar) {
+      sessionStorage.setItem(
+        SIDEBAR_SCROLL_TOP_KEY,
+        sidebar.scrollTop.toString()
+      );
+    }
+    return;
+  }
+  const item = activeItems[activeItems.length - 1];
+
+  let isItemVisibleAfterRestore = false;
+  if (sidebar) {
+    const itemRect = item.getBoundingClientRect();
+    const sidebarRect = sidebar.getBoundingClientRect();
+    console.log(itemRect, sidebarRect);
+    // Check if item is visible within the sidebar's viewport
+    isItemVisibleAfterRestore =
+      itemRect.top >= sidebarRect.top + SCROLL_THRESHOLD &&
+      itemRect.bottom <= sidebarRect.bottom - SCROLL_THRESHOLD;
   } else {
-    // Not visible.
+    const bounding = item.getBoundingClientRect();
+    isItemVisibleAfterRestore =
+      bounding.top >= 0 &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight);
+  }
+
+  // Not visible after restoring scroll, so scroll into view.
+  if (!isItemVisibleAfterRestore) {
     item.scrollIntoView({ block: "center", inline: "nearest" });
     document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
+
+  if (sidebar) {
+    sessionStorage.setItem(
+      SIDEBAR_SCROLL_TOP_KEY,
+      sidebar.scrollTop.toString()
+    );
+  }
 }
 
-// Watch for changes in the entire document
 function observeDocumentChanges() {
   const observer = new MutationObserver((mutationsList) => {
     for (const mutation of mutationsList) {
