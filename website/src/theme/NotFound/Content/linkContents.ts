fdb94d63b5ed3994037b50ed689d150b549d6dd1
RetricSu <inneverland2013@gmail.com>
2025-06-13 11:53:58 +0800

    feat: merge v2.21.0 into develop (#587)
    * Merge pull request #586 from joii2020/v2.dev.languanges
    
    Add JavaScript and Lua in Program Languages for Script
    
    * feat: update node related articles (#585)
    
    * feat: update node related articles
    
    * feat: Add introduction to node-configuration
    
    * chore: bump v2.21.0 (#588)
    
    ---------
    
    Co-authored-by: joii2020 <87224197+joii2020@users.noreply.github.com>
    Co-authored-by: yfeng2824 <140578792+yfeng2824@users.noreply.github.com>

--- website/src/theme/NotFound/Content/linkContents.ts
+++ website/src/theme/NotFound/Content/linkContents.ts
@@ -4,7 +4,7 @@ const suggestedLinks: CardLinks[] = [
   { label: "How CKB Works", link: "/docs/getting-started/how-ckb-works" },
   { label: "DApp Tutorials", link: "/docs/dapp/transfer-ckb" },
   { label: "Scripts (Smart contracts)", link: "/docs/script/intro-to-script" },
-  { label: "RPCs & Nodes", link: "/docs/node/rpcs" },
+  { label: "Run a Node", link: "/docs/node/node-overview" },
   { label: "Mining", link: "/docs/mining/guide" },
   {
     label: "Tech Explanation",
