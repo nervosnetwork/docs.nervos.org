426ad894ba15ef3f46808f1f8257da37ef234841
joii2020 <joii@cryptape.com>
2025-06-17 18:07:11 +0800

    Add TS example about call ckb-crypto-service

---
+++ examples/ckb-ts-script/packages/on-chain-script-tests/src/ipc.test.ts
@@ -0,0 +1,48 @@
+import { Resource, Verifier } from "ckb-testtool";
+import { hashCkb, hexFrom, Hex, Transaction, WitnessArgs } from "@ckb-ccc/core";
+import { readFileSync } from "fs";
+
+import { createJSScript } from "./misc";
+
+const SCRIPT_CALL_CRYPTO_SERVICE = readFileSync('../../contracts/call-crypto-service/dist/index.bc')
+
+test('call-crypto-service success', () => {
+    const resource = Resource.default();
+    const tx = Transaction.default();
+
+    // child
+    const ckbCryptaService = resource.deployCell(
+        hexFrom(readFileSync("../../deps/ckb-crypto-service")),
+        tx,
+        false
+    );
+
+    const lockScript =
+        createJSScript(
+            resource,
+            tx,
+            hexFrom(SCRIPT_CALL_CRYPTO_SERVICE),
+            hexFrom("0x" + ckbCryptaService.codeHash.slice(2)));
+    const inputCell = resource.mockCell(
+        lockScript,
+        undefined,
+        "0xFF000000000000000000000000000000",
+    );
+    tx.inputs.push(Resource.createCellInput(inputCell));
+
+    tx.outputs.push(Resource.createCellOutput(lockScript, undefined));
+    tx.outputsData.push(hexFrom("0x"));
+    tx.witnesses.push(
+        hexFrom(
+            new WitnessArgs(
+                hashCkb([0, 0, 0, 0]),
+                undefined,
+                undefined,
+            ).toBytes(),
+        )
+    );
+
+    let verifier = Verifier.from(resource, tx);
+    verifier.verifySuccess(true);
+});
+
