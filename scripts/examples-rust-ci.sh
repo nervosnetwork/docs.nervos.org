e26df1507798f1d72339b8a0869d5167c1fcb0e5
Joii <joii@cryptape.com>
2025-08-06 18:23:29 +0800

    Fix CI

--- scripts/examples-rust-ci.sh
+++ scripts/examples-rust-ci.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-cd "$SCRIPT_DIR/../cd examples/rust-script-examples/"
+cd "$SCRIPT_DIR/../examples/rust-script-examples/"
+
+rustup component add rustfmt
+rustup target add riscv64imac-unknown-none-elf
 
 make fmt
 if [ $? -ne 0 ]; then
