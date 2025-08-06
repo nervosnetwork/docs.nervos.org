429a38736800905c9c03781a2d88366e770100b2
Joii <joii@cryptape.com>
2025-08-06 18:23:29 +0800

    Fix CI

--- scripts/examples-ts-ci.sh
+++ scripts/examples-ts-ci.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-cd "$SCRIPT_DIR/../cd examples/ckb-ts-script/"
+cd "$SCRIPT_DIR/../examples/ckb-ts-script/"
 
 pnpm install
 if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+pnpm format
+git diff --exit-code
+if [ $? -ne 0 ]; then
+    echo "Check if there are any changes after formatting";
     exit 1
 fi
 
