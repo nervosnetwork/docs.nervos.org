e310937005ed2387b758769b110d9bbe91ab8ec2
Joii <joii@cryptape.com>
2025-08-08 10:28:30 +0800

    Check format: simple-lock/ts-simple-lock

--- scripts/examples-simple-lock-ci.sh
+++ scripts/examples-simple-lock-ci.sh
@@ -15,6 +15,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+pnpm format
+git diff --exit-code
+if [ $? -ne 0 ]; then
+    echo "Check if there are any changes after formatting";
+    exit 1
+fi
+
 pnpm build
 if [ $? -ne 0 ]; then
     exit 1
