5fcaa383c275f3bddeac01ae011c531a95a3c662
Joii <joii@cryptape.com>
2025-08-08 10:18:20 +0800

    Add C script CI

---
+++ scripts/examples-c-ci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd "$SCRIPT_DIR/../examples/c-script-examples/"
+
+make all
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+make tests
+if [ $? -ne 0 ]; then
+    exit 1
+fi
