9db7cc6b5b5b9ac924d4f2aa276616843c9c25b7
joii2020 <joii@cryptape.com>
2025-09-16 10:46:01 +0800

    Remove check url on CI , And add checkurl script

--- scripts/website-ci.sh
+++ scripts/website-ci.sh
@@ -12,9 +12,3 @@ if [ $? -ne 0 ]; then
 fi
 
 yarn build
-
-# Used by Lychee to check if URLs are valid.
-# Because there are a large number of GitHub URLs, a direct check would fail (returning a 429 error due to too many requests in a short period of time).
-# This function converts GitHub URLs to api.github.com. GitHub's API can support more requests.
-grep -rho "https://github.com[^\"'<> ]*" build | sort -u > build/github-urls.txt
-node ../scripts/convert-github-urls.js build/github-urls.txt > build/github-api-urls.txt
